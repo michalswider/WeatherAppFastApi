589fa853d0d376392e091300ced64a08c4b6cd85
Michał Świderski <michalswiderski41@gmail.com>
2025-01-23 22:27:24 +0100

    Setup Docker and update requirements.txt

--- weatherapp/database.py
+++ weatherapp/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-POSTGRESQL_DATABASE_URL = "sqlite:///weather.db"
+POSTGRESQL_DATABASE_URL = os.getenv("POSTGRESQL_DATABASE_URL")
 
-engine = create_engine(
-    POSTGRESQL_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(POSTGRESQL_DATABASE_URL)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
